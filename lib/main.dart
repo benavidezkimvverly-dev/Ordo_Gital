b70fb183e1ac238fba1d5b21bde845080c4b5970
benavidezkimvverly-dev <kdlsbenavidez@catsu.edu.ph>
2026-04-13 15:10:34 +0800

    Updating the main.dart code and pubsec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,18 @@
-ÿþ
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const OrdoGitalApp());
+}
+
+class OrdoGitalApp extends StatelessWidget {
+  const OrdoGitalApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'OrdoGital',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: Center(child: Text('OrdoGital is running!'))),
+    );
+  }
+}
